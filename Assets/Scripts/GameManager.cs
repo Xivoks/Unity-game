6acf42b7967bd12f1779d22702371af5e0fb2e89
Janek <j.brunow@lastlevel.pl>
2022-12-05 22:33:47 +0100

    added complete level transition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,20 @@ public class GameManager : MonoBehaviour
 {
     private bool gameHasEnded = false;
     public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
     public void EndGame()
     {
         if (gameHasEnded == false)
         {
             gameHasEnded = true;
             Debug.Log("Game Over");
-            Invoke("Restart",restartDelay);
+            Invoke("Restart", restartDelay);
         }
     }
 
