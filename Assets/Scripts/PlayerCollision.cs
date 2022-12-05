5b173d52c11364b383c75eb517434f037a20f5bf
Janek <j.brunow@lastlevel.pl>
2022-12-05 22:57:09 +0100

    added sounds

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,15 +1,17 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
+    public Material material;
+    public AudioSource sound;
     private void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Obstacle")
         {
+            collisionInfo.gameObject.GetComponent<MeshRenderer>().material=material;
+            sound.PlayOneShot(sound.clip);
             movement.enabled = false;
             FindObjectOfType<GameManager>().EndGame();
         }
